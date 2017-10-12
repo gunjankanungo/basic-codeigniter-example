56b3aa8e6323777a2fbfbfd2dc7e1f78b222e246
gunjankanungo <gunjan.kanungo321@gmail.com>
2017-10-12 16:28:10 +0530

    REST API

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # basic-codeigniter-example
 Codeigniter Image Example
+
+Postman
+GET :- http://localhost/basic-codeigniter-example/RestGetController/contacts
+
+Postman
+POST:- http://localhost/basic-codeigniter-example/RestGetController/add_contact
+Headers: Content-Type, value=application/json
+Body: {"contact_name":"gunjan", "contact_address":"gunjankanungo.com", "contact_phone":"9993222403"}
---
+++ application/controllers/RestGetController.php
@@ -0,0 +1,57 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require(APPPATH . '/libraries/REST_Controller.php');
+
+/**
+ * Description of RestGet
+ *
+ * @author http://roytuts.com
+ */
+class RestGetController extends REST_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('ContactModel', 'cm');
+    }
+
+    function contacts_get() {
+        $contacts = $this->cm->get_contact_list();
+
+        if ($contacts) {
+            $this->response($contacts, 200);
+        } else {
+            $this->response(NULL, 404);
+        }
+    }
+
+    function contact_get() {
+        if (!$this->get('id')) {
+            $this->response(NULL, 400);
+        }
+
+        $contact = $this->cm->get_contact($this->get('id'));
+
+        if ($contact) {
+            $this->response($contact, 200); // 200 being the HTTP response code
+        } else {
+            $this->response(NULL, 404);
+        }
+    }
+
+    function add_contact_post() {
+        $contact_name = $this->post('contact_name');
+        $contact_address = $this->post('contact_address');
+        $contact_phone = $this->post('contact_phone');
+        
+        $result = $this->cm->add_contact($contact_name, $contact_address, $contact_phone);
+
+        if ($result === FALSE) {
+            $this->response(array('status' => 'failed'));
+        } else {
+            $this->response(array('status' => 'success'));
+        }
+    }
+
+}
